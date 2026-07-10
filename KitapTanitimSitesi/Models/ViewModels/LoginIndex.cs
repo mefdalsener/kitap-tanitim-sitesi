e04ec5e18c92efca333c874ad31492530de7d817
Muhammed Efdal Şener <m.efdal66@hotmail.com>
2026-07-10 14:17:46 +0300

    Model alanlarına Türkçe etiketler eklendi
    LoginIndex.cs ve SignupIndex.cs dosyalarındaki form alanlarına [Display] attribute'u ile Türkçe görünen adlar tanımlandı. Login.cshtml dosyasında ise sadece biçimsel bir düzenleme yapıldı, işlevsel bir değişiklik yok.

--- KitapTanitimSitesi/Models/ViewModels/LoginIndex.cs
+++ KitapTanitimSitesi/Models/ViewModels/LoginIndex.cs
@@ -5,10 +5,12 @@ namespace KitapTanitimSitesi.Models.ViewModels
     public class LoginIndex
     {
         [Required(ErrorMessage = "Kullanıcı adı veya e-posta gerekli.")]
+        [Display(Name = "Kullanıcı Adı ya da E-posta")]
         public string UsernameOrEmail { get; set; }
 
         [Required(ErrorMessage = "Şifre gerekli.")]
         [DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
         public string Password { get; set; }
     }
 }
--- KitapTanitimSitesi/Models/ViewModels/SignupIndex.cs
+++ KitapTanitimSitesi/Models/ViewModels/SignupIndex.cs
@@ -5,16 +5,20 @@ namespace KitapTanitimSitesi.Models.ViewModels
     public class SignupIndex
     {
         [Required, MaxLength(50)]
+        [Display(Name = "Kullanıcı Adı")]
         public string Username { get; set; }
 
         [Required, EmailAddress, MaxLength(100)]
+        [Display(Name = "E-Posta")]
         public string Email { get; set; }
 
         [Required, DataType(DataType.Password)]
+        [Display(Name = "Şifre")]
         public string Password { get; set; }
 
         [Required, DataType(DataType.Password)]
         [Compare("Password", ErrorMessage = "Şifreler eşleşmiyor.")]
+        [Display(Name = "Şifreyi Tekrarlayın")]
         public string PasswordConfirm { get; set; }
     }
 }
